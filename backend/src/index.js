a85eabdc79886fd416d0cde05cad69ed26eed3dd
GersonSalvador <zecoevane@yahoo.com.br>
2020-03-26 23:16:42 -0300

    Backend

---
+++ backend/src/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+app.use(cors()); //{origin: 'https://meuapp.com'} allows origin URL only
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
